de5c7225cc75fe54956cb21ecc5cc3819da1c50d
Hyeonyun <khy7571@hanmail.net>
2017-04-18 20:00:06 +0900

    commit_4

--- SWhomework.java
+++ SWhomework.java
@@ -3,5 +3,7 @@ public class SWhomework {
       System.out.println("Git_Homework 3");
             System.out.println("commit 1 create");
             System.out.println("commit 2 create");
-                      System.out.println("commit 2 create");   }
+                      System.out.println("commit 3 create"); 
+System.out.println("commit 4 create");
+                      }
 }
